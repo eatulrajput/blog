788d9708217f1f6d5d5f82da24bd683ce678bfd5
Atul Rajput <eatulrajput@gmail.com>
2024-10-18 16:27:55 +0530

    Improve Responsiveness

--- my-blog/src/components/Navbar.js
+++ my-blog/src/components/Navbar.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // We'll create this CSS file for styling
+import './Navbar.css'; // Make sure this file has the necessary CSS
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faSun, faMoon, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = ({ toggleTheme, theme }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to track menu visibility
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Toggle the menu state between open and close
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
         <Link to="/">My Blog</Link>
       </div>
-      <ul className="navbar-links">
+      
+      <button className="menu-toggle-btn" onClick={toggleMenu}>
+        {/* Switch between hamburger and close icon based on menu state */}
+        <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
+      </button>
+      
+      <ul className={`navbar-links ${isMenuOpen ? 'open' : ''}`}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={toggleMenu}>Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about" onClick={toggleMenu}>About</Link>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <Link to="/contact" onClick={toggleMenu}>Contact</Link>
         </li>
         <li>
           <button id="theme-toggle" className="theme-toggle-btn" onClick={toggleTheme}>
