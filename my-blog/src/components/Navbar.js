e9eab888b4023bcf976acdd908a26aa243aac7b4
Atul Rajput <eatulrajput@gmail.com>
2024-10-18 16:16:27 +0530

    Add scroll to top button

--- my-blog/src/components/Navbar.js
+++ my-blog/src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; // We'll create this CSS file for styling
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
-const Navbar = () => {
+const Navbar = ({ toggleTheme, theme }) => {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
@@ -17,6 +19,15 @@ const Navbar = () => {
         </li>
         <li>
           <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <button id="theme-toggle" className="theme-toggle-btn" onClick={toggleTheme}>
+            {theme === 'dark-mode' ? (
+              <FontAwesomeIcon icon={faSun} /> // Show sun icon for dark mode
+            ) : (
+              <FontAwesomeIcon icon={faMoon} /> // Show moon icon for light mode
+            )}
+          </button>
         </li>
       </ul>
     </nav>
