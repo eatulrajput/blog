778692aa1a418a1af02635965735ff9e71c6e4ec
Atul Rajput <eatulrajput@gmail.com>
2024-10-18 11:59:47 +0530

    Add Page

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -2,20 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BlogList from './components/BlogList';
 import BlogPost from './components/BlogPost';
-import Navbar from './components/Navbar'; // Import Navbar
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import About from './components/About';
+import Contact from './components/Contact'; // Import the Contact component
 import './index.css';
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Navbar /> {/* Add the Navbar component here */}
+        <Navbar />
         <Routes>
           <Route path="/" element={<BlogList />} />
           <Route path="/post/:id" element={<BlogPost />} />
-          <Route path="/about" element={<h2>About Us</h2>} />
-          <Route path="/contact" element={<h2>Contact Us</h2>} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} /> {/* Contact page route */}
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
